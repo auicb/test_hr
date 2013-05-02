9453e64e86fc1d3b81d7634c488cc9f1359c7b5c
au <patch2527@gmail.com>
2013-05-02 11:07:59 +0700

    start

---
+++ db/empstore.php
@@ -0,0 +1,22 @@
+﻿{"data": {
+  "records": [   
+    {
+		"id": "1",
+		"firstName": "นางพรชนก",
+		"middleName": "AA",
+		"lastName": "ขันใส",
+		"empId": "0001",
+		"photograph": "AA"
+    },
+	{
+		"id": "2",
+		"firstName": "นายคันศร",
+		"middleName": "AA",
+		"lastName": "คมภักดี",
+		"empId": "0002",
+		"photograph": "AA"
+	}
+	]
+	}
+  
+ }
